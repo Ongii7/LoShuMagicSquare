8799e3a1d53a7a6d97db3aac1fac72d4956294b9
Ongii7 <ongii.k18@gmail.com>
2022-10-22 19:55:15 -0700

    initial push with outline

---
+++ *.c
@@ -0,0 +1,11 @@
+/**
+ * Create program and create function that has a Lo Shu Magic Square array and one that is not, 
+ * if array indexes for cols m in rows n and rows n and n reversed in cols m add up to 15 then print that it is a Lo Shu Magic Square.
+ * else then print that it is not a Lo Shu Magic Square.
+ * create a 1d array 1-9, then for length of rows x cols,
+ * choose a random element in the array, have a temp variable hold current index element, swap current index with random element, then give the index of random element the temp variable.
+ * after randomization, copy values from 1d array into a new 2d array.
+ * if Lo Shu Magic Square array, then print total number of times it randomized times 9, and how many times it randomized.
+ * then print the successful array in visual format.
+ * else, repeat randomization loop and increase the counter for how many times it randomized by one.
+ **/
